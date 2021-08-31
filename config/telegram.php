a1a759763a5c4d7730dfcc8e31643319fbffa292
wirakw <kusumawira98@gmail.com>
2021-08-31 19:52:16 +0700

    change config

--- config/telegram.php
+++ config/telegram.php
@@ -38,7 +38,7 @@ return [
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
-                App\Telegram\Commands\HelpCommand::class,
+                // App\Telegram\Commands\HelpCommand::class,
             ],
         ],
 
@@ -116,7 +116,7 @@ return [
     |
     */
     'commands'                     => [
-        Telegram\Bot\Commands\HelpCommand::class,
+        App\Telegram\Commands\HelpCommand::class,
     ],
 
     /*
