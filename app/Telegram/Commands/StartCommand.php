ce1dcb9e20250e7e09944b69927f95df0217b9d0
wirakw <kusumawira98@gmail.com>
2021-08-31 23:02:06 +0700

    command

---
+++ app/Telegram/Commands/StartCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Telegram\Commands;
+
+use Throwable;
+use Telegram;
+use Telegram\Bot\Commands\Command;
+
+/**
+ * Class StartCommand.
+ */
+class StartCommand extends Command
+{
+    protected string $description = 'Start command to process initial request!';
+
+    /**
+     * Execute the bot command.
+     */
+    public function handle()
+    {
+        $message = $this->getUpdate()->getMessage();
+        $firstName = $message->from->first_name;
+
+        $text = "Hello, $firstName! Welcome to our bot!\nType /help to get a list of available commands.";
+
+        $this->bot->sendMessage([
+            'chat_id' => $message->chat->id,
+            'text'    => $text,
+        ]);
+    }
+
+   /**
+    * Triggered on failure.
+    *
+    * @param array     $arguments
+    * @param Throwable $exception
+    */
+    public function failed(array $arguments, Throwable $exception)
+    {
+        //
+    }
+}
--- config/telegram.php
+++ config/telegram.php
@@ -38,7 +38,8 @@ return [
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
-                App\Telegram\Commands\HelpCommand::class,
+                'start' => App\Telegram\Commands\StartCommand::class,
+                'help' => App\Telegram\Commands\HelpCommand::class,
             ],
         ],
 
