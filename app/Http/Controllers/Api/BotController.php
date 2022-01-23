3a582ee4cd543ddd390b67f0e432eb334b340630
wira kusuma wardana <wira.kusuma.ext@huawei.com>
2022-01-23 20:50:46 +0700

    start

--- app/Http/Controllers/Api/BotController.php
+++ app/Http/Controllers/Api/BotController.php
@@ -39,12 +39,15 @@ class BotController extends Controller
         $pesan = Telegram::commandsHandler(true);
         $isCommand = substr($pesan['message']['text'],0,1);
         if ($isCommand != "/") {
-            $Str1 = preg_replace('/[\x00-\x1F\x7F-\xFF]/', '', $pesan['message']['text']); //normalisasi
+            $str = preg_replace('/[\x00-\x1F\x7F-\xFF]/', '', $pesan['message']['text']); //normalisasi
             Telegram::sendMessage([
                         'chat_id' => $pesan['message']['chat']['id'],
                         'text' => "Sorry, command <b>" . $pesan['message']['text'] . "</b> wasn't found :/",
                         'parse_mode' => 'HTML',
                     ]);
+            if ($str == 'start') {
+                $this->sendMessage();
+            }
         }
 
         return response()->json([
